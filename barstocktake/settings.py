e211bda8269bd1f9fe99b8fdd31cb48d0bb00354
Andrew <andrewmreiblein013@gmail.com>
2021-02-21 13:59:54 +0200

    database creation

--- barstocktake/settings.py
+++ barstocktake/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'barstocktake.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stocktakedb',
+        "USER":'postgres',
+        'PASSWORD':'l33tc0d3r',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
